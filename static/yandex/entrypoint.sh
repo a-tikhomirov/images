2f17082583fdb0071dd7fe207ac5cf92d6718707
a-tikhomirov <andrey.tikhomirov.88@gmail.com>
2022-04-06 13:52:11 +0300

    added devtools for yandex browser

--- static/yandex/entrypoint.sh
+++ static/yandex/entrypoint.sh
@@ -25,6 +25,9 @@ clean() {
   if [ -n "$X11VNC_PID" ]; then
     kill -TERM "$X11VNC_PID"
   fi
+  if [ -n "$DEVTOOLS_PID" ]; then
+    kill -TERM "$DEVTOOLS_PID"
+  fi
   if [ -n "$PULSE_PID" ]; then
     kill -TERM "$PULSE_PID"
   fi
@@ -52,8 +55,13 @@ fi
 /usr/bin/fileserver &
 FILESERVER_PID=$!
 
+/usr/bin/devtools &
+DEVTOOLS_PID=$!
+
 DISPLAY="$DISPLAY" /usr/bin/xseld &
 XSELD_PID=$!
+
+while ip addr | grep inet | grep -q tentative > /dev/null; do sleep 0.1; done
 
 mkdir -p ~/pulse/.config/pulse
 echo -n 'gIvST5iz2S0J1+JlXC1lD3HWvg61vDTV1xbmiGxZnjB6E3psXsjWUVQS4SRrch6rygQgtpw7qmghDFTaekt8qWiCjGvB0LNzQbvhfs1SFYDMakmIXuoqYoWFqTJ+GOXYByxpgCMylMKwpOoANEDePUCj36nwGaJNTNSjL8WBv+Bf3rJXqWnJ/43a0hUhmBBt28Dhiz6Yqowa83Y4iDRNJbxih6rB1vRNDKqRr/J9XJV+dOlM0dI+K6Vf5Ag+2LGZ3rc5sPVqgHgKK0mcNcsn+yCmO+XLQHD1K+QgL8RITs7nNeF1ikYPVgEYnc0CGzHTMvFR7JLgwL2gTXulCdwPbg=='| base64 -d>~/pulse/.config/pulse/cookie
@@ -61,7 +69,7 @@ HOME=$HOME/pulse pulseaudio --start --exit-idle-time=-1
 HOME=$HOME/pulse pactl load-module module-native-protocol-tcp
 PULSE_PID=$(ps --no-headers -C pulseaudio -o pid | sed -r 's/( )+//g')
 
-/usr/bin/xvfb-run -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/fluxbox -display "$DISPLAY" -log /dev/null  2>/dev/null &
+/usr/bin/xvfb-run -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/fluxbox -display "$DISPLAY" -log /dev/null 2>/dev/null &
 XVFB_PID=$!
 
 retcode=1
